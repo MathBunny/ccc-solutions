527a6302b0261e68f2fd2586c3773e812fe39919
MathBunny <horatiulazu@gmail.com>
2015-09-06 22:18:24 -0400

    Cleaned S4 2011

--- Senior 2011/S4/Main.java
+++ Senior 2011/S4/Main.java
@@ -3,11 +3,6 @@ Made by Horatiu Lazu */
 
 import java.io.*;
 import java.util.*;
-import java.lang.*;
-import java.awt.*;
-import java.awt.geom.*;
-import java.math.*;
-
 
 class Main{
 	int sum = 0;
@@ -18,24 +13,20 @@ class Main{
 	
 	public void conditional(int [] patients, int [] bloodSupply, int patientIndex, int bloodSupplyIndex, boolean isNegative){
 		if (patients[patientIndex] > bloodSupply[bloodSupplyIndex]){
-			//System.out.println("ADDED: " + bloodSupply[bloodSupplyIndex] + "from: " + patientIndex +" using" + bloodSupplyIndex);
 			sum += bloodSupply[bloodSupplyIndex];
 			patients[patientIndex] -= bloodSupply[bloodSupplyIndex];
 			bloodSupply[bloodSupplyIndex] = 0;	
 		}else{
-			//System.out.println("ADDED2: " + patients[patientIndex] + "from: " + patientIndex +" using" + bloodSupplyIndex);
 			sum += patients[patientIndex];
 			bloodSupply[bloodSupplyIndex] -= patients[patientIndex];	
 			patients[patientIndex] = 0;
 		}	
 		if (!isNegative){
 			if (patients[patientIndex] > bloodSupply[bloodSupplyIndex-1]){
-				//System.out.println("ADDED: " + bloodSupply[bloodSupplyIndex-1] + "from: " + patientIndex +" using" + (bloodSupplyIndex-1));
 				sum += bloodSupply[bloodSupplyIndex-1];
 				patients[patientIndex] -= bloodSupply[bloodSupplyIndex-1];
 				bloodSupply[bloodSupplyIndex-1] = 0;	
 			}else{
-				//System.out.println("ADDED2: " + patients[patientIndex] + "from: " + patientIndex +" using" + (bloodSupplyIndex-1));
 				sum += patients[patientIndex];
 				bloodSupply[bloodSupplyIndex-1] -= patients[patientIndex];	
 				patients[patientIndex] = 0;
@@ -47,24 +38,18 @@ class Main{
 		try{
 			BufferedReader in;
 			in = new BufferedReader (new InputStreamReader (System.in)); //Used for CCC
-			//in = new BufferedReader(new FileReader("A.in")); //Used for JDCC & others
 			
 			StringTokenizer st = new StringTokenizer(in.readLine());
-			
 			int [] bloodSupply = new int[8];
 			for(int i = 0; i < bloodSupply.length; i++)
 				bloodSupply[i] = Integer.parseInt(st.nextToken());
-				
-			
+	
 			int [] patients = new int[8];
 			st = new StringTokenizer(in.readLine());
 			for(int i = 0; i < 8; i++)
 				patients[i] = Integer.parseInt(st.nextToken());
 			
-			
 			/* Implementation of efficient algorithm */
-			//method signature:
-			//public void conditional(int [] patients, int [] bloodSupply, int patientIndex, int bloodSupplyIndex, boolean isNegative)
 			conditional(patients,bloodSupply, 0, 0,true);
 			conditional(patients,bloodSupply,1,1,false);
 			
@@ -85,53 +70,7 @@ class Main{
 				conditional(patients,bloodSupply,7,i,true);
 			
 			System.out.println(sum);
-			
-			
-			/* Type O patient */
-			/*
-			if (oNegative > patients[0]){
-				sum += patients[0];
-				oNegative -= patients[0];
-				patients[0] = 0;	
-			}
-			else{
-				sum += oNegative;
-				patients[0] -= oNegative;
-				oNegative = 0;	
-			}
-			
-			if (oNegative != 0){
-				if (oNegative > patients[1]){
-					sum += patients[1];
-					oNegative -= patients[1];
-					patients[1] = 0;	
-				}
-				else{
-					patients[1] -= oNegative;
-					sum += oNegative;
-					oNegative = 0;
-				}
-			}	
-			if (oPositive > patients[1]){
-				sum += patients[1];
-				oPositive -= patients[1];
-				patients[1] = 0;	
-			}
-			else{
-				sum += oPositive;
-				patients[1] -= oPositive;
-				oPositive = 0;
-			}	
-			
-			
-			TYPE A: if (aNegative > patients[2]){
-					
-			}
-			*/
-			
-			
-			//out.close();
-			
+
 		}
 		catch(IOException e){
 			System.out.println("IO: General");
